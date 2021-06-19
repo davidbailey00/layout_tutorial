5b085c787710f9ea443c6d37a234d18fbf4b9b0e
David Bailey <davidbailey00@outlook.com>
2021-06-19 22:15:28 +0100

    Implement title row

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,41 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.grey[500]),
+              ),
+            ],
+          ),
+        ),
+        MergeSemantics(
+          child: Row(children: [
+            Icon(Icons.star, color: Colors.red[500]),
+            Text('41', semanticsLabel: '41 stars'),
+          ]),
+        ),
+      ]),
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+        body: Column(children: [titleSection]),
       ),
     );
   }
