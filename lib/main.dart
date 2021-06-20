8ea6e860761188291d8e2ad64efddaff73c1c1ce
David Bailey <davidbailey00@outlook.com>
2021-06-20 11:41:15 +0100

    Use ListView instead of Column to enable scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(children: [
+        body: ListView(children: [
           Image.asset(
             'images/lake.jpg',
             width: 600,
